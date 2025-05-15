26bd1aa62b5701fc8716d89c4cb643998a68fd35
YPINPIN <yping611@gmail.com>
2025-05-16 00:20:49 +0800

    add custom bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
 <template>
   <div class="container">
     <h1>BS5 Vue WebLayout1</h1>
-    <button class="btn btn-outline-primary">primary button</button>
+    <div class="row mt-3 g-1">
+      <div class="d-grid col-6">
+        <button class="btn btn-primary d-block">primary button</button>
+      </div>
+      <div class="d-grid col-6">
+        <button class="btn btn-outline-secondary">
+          outline-secondary button
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
