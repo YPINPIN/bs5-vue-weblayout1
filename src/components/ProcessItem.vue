686494367cc7c1db3945b692e04ac38493ceaa5b
YPINPIN <yping611@gmail.com>
2025-05-22 00:58:22 +0800

    add ProductProcess

---
+++ src/components/ProcessItem.vue
@@ -0,0 +1,79 @@
+<script setup>
+import dayjs from 'dayjs';
+const { id, title, description, picture, date } = defineProps({
+  id: Number,
+  title: String,
+  description: String,
+  picture: String,
+  date: Date,
+});
+
+const getImageUrl = (name) => {
+  return new URL(`../assets/images/${name}.jpg`, import.meta.url).href;
+};
+
+const formatDate = (date) => {
+  return dayjs(date).format('YYYY-M-D');
+};
+</script>
+
+<template>
+  <div class="wrapper bg-white rounded-1 d-flex overflow-hidden">
+    <img
+      class="object-fit-cover process-img"
+      :src="getImageUrl(picture)"
+      :alt="`process-picture-${picture}`"
+    />
+    <div class="p-3 pb-2 p-sm-3 d-flex flex-column">
+      <div class="fw-bold c-fs-16 c-text-dark2 process-title">{{ title }}</div>
+      <span class="c-fn-num c-fs-14 text-secondary mb-2 process-date">{{
+        formatDate(date)
+      }}</span>
+      <p class="d-none d-sm-block c-fs-14 text-secondary process-desc">
+        {{ description }}
+      </p>
+      <a
+        href="#"
+        class="text-decoration-none c-fn-num c-fs-16 c-fs-lg-14 c-text-hover fw-bold process-link"
+        >MORE</a
+      >
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper {
+  border: 1px solid #e5e5e5;
+}
+.process-img {
+  max-width: g-fn.rem(135px);
+}
+.process-title {
+  letter-spacing: g-fn.rem(0.7px);
+}
+.process-date {
+  letter-spacing: g-fn.rem(0.49px);
+  line-height: g-fn.rem(24px);
+}
+.process-desc {
+  letter-spacing: g-fn.rem(0.7px);
+  line-height: g-fn.rem(20px);
+  text-align: justify;
+}
+.process-link {
+  letter-spacing: g-fn.rem(0.8px);
+  line-height: g-fn.rem(28px);
+}
+@media screen and (min-width: 576px) {
+  .process-img {
+    max-width: g-fn.rem(220px);
+  }
+  .process-title {
+    letter-spacing: g-fn.rem(0.8px);
+  }
+  .process-link {
+    letter-spacing: g-fn.rem(0.7px);
+    line-height: g-fn.rem(24px);
+  }
+}
+</style>
--- src/components/ProductProcess.vue
+++ src/components/ProductProcess.vue
@@ -1,3 +1,44 @@
+<script setup>
+import ProcessItem from './ProcessItem.vue';
+
+const processData = [
+  {
+    id: 1,
+    title: '高雄體驗會｜免費參加送帆布袋',
+    description:
+      '大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！',
+    picture: 'news01',
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: '台北體驗會｜免費參加送帆布袋',
+    description:
+      '大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！',
+    picture: 'news02',
+    date: new Date(),
+  },
+  {
+    id: 3,
+    title: '台中體驗會｜免費參加送帆布袋',
+    description:
+      '大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！',
+    picture: 'news03',
+    date: new Date(),
+  },
+];
+</script>
+
 <template>
-  <div class="py-3 py-lg-4">process-tab-pane...</div>
+  <div class="py-3 py-lg-4">
+    <div class="row my-3 my-lg-4">
+      <div
+        v-for="(data, index) in processData"
+        :key="data.id"
+        :class="['col-12', { 'mb-3': index != processData.length - 1 }]"
+      >
+        <ProcessItem v-bind="data" />
+      </div>
+    </div>
+  </div>
 </template>
