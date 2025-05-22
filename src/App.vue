9cae1d9c25cf099e60b24b5f729648ad8d07ecc3
YPINPIN <yping611@gmail.com>
2025-05-23 01:39:36 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,25 @@ import ProductContent from './components/ProductContent.vue';
     <Product />
     <ProductContent />
   </main>
+  <footer
+    class="c-bg-dark2 text-white d-flex justify-content-center align-items-center"
+  >
+    <div class="c-fs-14 c-fs-md-16">
+      Copyright ©2025 拼拼 All rights reserved.
+    </div>
+  </footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+footer {
+  height: g-fn.rem(54px);
+  letter-spacing: 0.18px;
+}
+
+@media screen and (min-width: 768px) {
+  footer {
+    height: g-fn.rem(88px);
+    letter-spacing: 0.2px;
+  }
+}
+</style>
