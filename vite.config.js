2b04988679e6d3d094a1ca53b5b0592d77344790
YPINPIN <yping611@gmail.com>
2025-05-24 00:49:12 +0800

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/bs5-vue-weblayout1/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
